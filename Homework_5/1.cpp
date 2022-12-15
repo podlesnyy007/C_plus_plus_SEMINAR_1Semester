33c1a4a5b62d2bbce47e49b288cc1260523306e2
podlesnyy007 <114169264+podlesnyy007@users.noreply.github.com>
2022-12-16 00:07:29 +0400

    Create 1.cpp

---
+++ Homework_5/1.cpp
@@ -0,0 +1,19 @@
+/*
+Прочитать массив из n чисел, заменить последние 3 элемента на -1 и вывести
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "n = "; cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = n - 1; i > n - 4; i--)
+		a[i] = -1;
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+}
